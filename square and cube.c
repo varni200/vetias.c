3dac8ab3a9fe69c476df098518bc43fe3df26a01
varni200 <varnigags23csa@vetias.ac.in>
2025-05-12 15:49:52 +0530

    Create square and cube.c

---
+++ square and cube.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+int n;
+int s;
+int c;
+
+printf("Enter  a number:");
+scanf("%d",&n);
+printf("Enter square:");
+scanf("%d",&s);
+printf("Enter cube:");
+scanf("%d",&c);
+int square = 4;
+int cube = 6;
+printf("square = %d, cube = %d",square,cube);
+}
