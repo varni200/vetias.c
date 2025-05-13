b5877c3bd82d144c781ad87898604833d556d17a
varni200 <varnigags23csa@vetias.ac.in>
2025-05-13 14:24:59 +0530

    Create max two num.c

---
+++ max two num.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+ int a;
+ int b;
+ int max;
+ printf("Enter two number:");
+ scanf("%d %d" ,&a,&b);
+ max =(a>b)?a:b;
+ printf("the maximum number is :%d\n",max);
+ 
+ return 0;
+}
+
+    
+
