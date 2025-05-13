717ba18a2d377658714368de8170ae37c62c2c52
varni200 <varnigags23csa@vetias.ac.in>
2025-05-13 14:32:05 +0530

    Create leapyear.c

---
+++ leapyear.c
@@ -0,0 +1,23 @@
+ 
+
+ 
+
+ 
+#include <stdio.h>
+int main()
+
+{
+ int a;
+ scanf("%d",&a);
+   if(a%4){
+ printf("given number is leapyear");
+   } else {
+       printf("given number is not leapyear");
+   }
+ 
+ 
+ return 0;
+}
+
+    
+
