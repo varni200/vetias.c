67b7c6e5650997307a1a6b850735b8599611bdb9
varni200 <varnigags23csa@vetias.ac.in>
2025-05-14 15:29:36 +0530

    Create positive negative.c

---
+++ positive negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+ int a;
+    scanf("%d", &a);
+    if(a>0)
+    { 
+        printf("Positive");
+    }
+    else if(a<0)
+    {
+        
+    printf("Negative");
+    }
+    else
+    {
+    
+        printf("Zero");
+    }
+    
+    
+    return 0;
+}
