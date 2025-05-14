d8bf73e6219a94941a89e6d9da35d3f2c75a21e5
varni200 <varnigags23csa@vetias.ac.in>
2025-05-14 15:54:00 +0530

    Create equal not equal.c

---
+++ equal not equal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+     int a,b;
+  
+    scanf("%d %d" ,&a,&b);
+    
+    if (a==b){
+      printf("EQUAL");
+    }
+    else { printf("NOT EQUAL");
+    
+    }
+    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
