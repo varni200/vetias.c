b9ee30a06cb30ef7e57d3e91cd724ba3f21ea2fa
varni200 <varnigags23csa@vetias.ac.in>
2025-05-14 15:55:06 +0530

    Create odd even cond.c

---
+++ odd even cond.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+      int a;
+      scanf("%d" ,&a);
+          if(a%2!=0)
+          {
+              printf("ODD");
+          }  else{
+              printf("EVEN");
+          }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
